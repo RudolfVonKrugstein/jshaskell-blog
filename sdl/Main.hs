a02f1b160f520027289e7c59a9b9b54002e85e9f
Nathan <nathan.huesken@posteo.de>
2012-09-27 09:45:58 +0200

    Updated sdl test (working now)

--- sdl/Main.hs
+++ sdl/Main.hs
@@ -31,24 +31,26 @@ start rc@(RunConfig update interval keyUp keyDown) = withInit [InitEverything] $
       time <- getTicks
       -- delay
       unless (time >= startTime + interval) $ delay $ startTime + interval - time
+      -- update
+      update
       -- events
       quit <- handleEvents
       -- update and continiue
       update
       unless quit $ mainLoop $ min (startTime + interval) time
 
-    handleEvents ::IO Bool
+    handleEvents :: IO Bool
     handleEvents = do
-      events <- liftM ( takeWhile (/= NoEvent)) $ sequence $ repeat pollEvent
-      mapM_ handleEvent events
-      -- return value
-      return $ Quit `elem` events
+      event <- pollEvent
+      case event of
+         Quit    -> return True
+         NoEvent -> return False
+         e       -> handleEvent e >> handleEvents
       where
         handleEvent :: Event -> IO ()
         handleEvent e = case e of
-          KeyDown sym -> keyDown $ (fromIntegral . U.fromEnum . symKey $ sym)
-          KeyUp   sym -> keyUp   $ (fromIntegral . U.fromEnum . symKey $ sym)
+          KeyDown sym -> keyDown (fromIntegral . U.fromEnum . symKey $ sym)
+          KeyUp   sym -> keyUp   (fromIntegral . U.fromEnum . symKey $ sym)
           _           -> return ()
     
-
 
